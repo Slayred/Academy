05498e5e6f28d78d75a0fa85ba612e3c1bde5e78
slayred <aleksch1989@gmail.com>
2020-10-22 20:10:50 +0300

    first

---
+++ src/mda/Mda9.java
@@ -0,0 +1,47 @@
+package mda;
+
+import java.util.Scanner;
+
+public class Mda9 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int[][] ar = new int[a][a];
+		int s = 1;
+		
+		over: for (int k = 0; s <= a*a; k++) {
+		for(int i = 0; i<=ar.length - 1; i++) {
+			if (ar[k][i] == 0) {
+				ar[k][i] = s;
+				s++;
+				}
+		}
+		for (int i = 0; i<=ar.length - 1; i++) {
+			if (ar[i][a -(k+1)] == 0) {
+			ar[i][a- (k+1)] = s;
+			s++;
+			}
+		}
+		for (int i = ar[a-1].length - 1; i >= 0 ; i--) {
+			if (ar[a -(k+1)][i] == 0) {
+				ar[a-(k+1)][i] = s;
+				s++;
+				}
+		}
+		
+		for (int i = ar.length - 1; i >= 0; i--) {
+			if (ar[i][k] == 0) {
+				ar[i][k] = s;
+				s++;
+			}
+		}
+		}		
+		for(int i = 0; i <= ar.length - 1; i++) {
+			for (int j = 0; j <= ar[i].length - 1; j++) {
+				System.out.print(ar[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+	
+}
