3eb1e2e9251bcf9b3180f239d860e3b901fe13d1
slayred <aleksch1989@gmail.com>
2020-10-22 22:09:01 +0300

    imposible

--- src/mda/Mda10.java
+++ src/mda/Mda10.java
@@ -8,12 +8,28 @@ public class Mda10 {
 		String inp = scan.nextLine();
 		char[][] ar = new char[3][3];
 		int k = 0;
+		int x = 0;
+		int o = 0;
 		for(int i = 0; i <= ar.length-1; i++) {
 			for (int j = 0; j <= ar[i].length -1; j++) {
 				ar[i][j] = inp.charAt(k);
 				k++;
 			}
 		}
+		for(int i = 0; i <= ar.length-1; i++) { // for array
+			for (int j = 0; j <= ar[i].length -1; j++) {
+				if (ar[i][j] == 'X') {
+					x++;
+				} else if (ar[i][j] == 'O') {
+					o++;
+				}
+			}
+			if ( Math.abs(x - o) > 1) {
+				System.out.println("Impossible");
+				break;
+			}
+		}
+		
 		
 /*		for(int i = 0; i <= ar.length-1; i++) { // for array
 			for (int j = 0; j <= ar[i].length -1; j++) {
@@ -21,11 +37,11 @@ public class Mda10 {
 			}
 		}*/
 		
-		/*for(int i = 0; i <= ar.length - 1;i++) {
+		for(int i = 0; i <= ar.length - 1;i++) {
 			for (int j = 0; j <= ar[i].length-1; j++) {
 				System.out.print(ar[i][j] + " ");
 			}
 			System.out.println();
-		}*/
+		}
 	}
 }
