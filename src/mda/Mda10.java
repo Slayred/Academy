7535e82fb31266f2ce3811915abfbd155902010c
slayred <aleksch1989@gmail.com>
2020-10-22 22:01:13 +0300

    print array

--- src/mda/Mda10.java
+++ src/mda/Mda10.java
@@ -1,7 +1,24 @@
 package mda;
 
+import java.util.Scanner;
+
 public class Mda10 {
 	public static void main(String[] args) {
-		
+		Scanner scan = new Scanner(System.in);
+		String inp = scan.nextLine();
+		char[][] ar = new char[3][3];
+		int k = 0;
+		for(int i = 0; i <= ar.length-1; i++) {
+			for (int j = 0; j <= ar[i].length -1; j++) {
+				ar[i][j] = inp.charAt(k);
+				k++;
+			}
+		}
+		for(int i = 0; i <= ar.length - 1;i++) {
+			for (int j = 0; j <= ar[i].length-1; j++) {
+				System.out.print(ar[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 }
