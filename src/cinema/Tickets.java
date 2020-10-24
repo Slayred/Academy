6c59a34070a8b49821400c9cdd29dbe07e61988b
slayred <aleksch1989@gmail.com>
2020-10-24 23:24:42 +0300

    4/5

--- src/cinema/Tickets.java
+++ src/cinema/Tickets.java
@@ -1,10 +1,62 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Tickets {
+	public static void printAr(char ar[][]) {
+		for(int i = 0; i <= ar.length-1; i++) { 
+			for (int j = 0; j <= ar[i].length -1; j++) {
+				System.out.print(ar[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	public static char[][] greatArr(int a, int b){
 
+		char ar[][] = new char[a+1][b+1];
+		for (int i = 1; i <=ar.length - 1; i++) {
+			ar[i][0] = (char)(48+i);			
+		}
+		for (int i= 1; i <=ar[0].length - 1;i++) {
+			ar[0][i] = (char)(48+i);	
+		}
+		for (int i = 1; i<=ar.length - 1; i++) {
+			for(int j = 1; j<=ar[i].length-1; j++) {
+				if (ar[i][j] != ' ') {
+					ar[i][j] = 'S';
+				}
+			}
+		}
+		return ar;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number of rows:");
+		int a = scan.nextInt();
+		System.out.println("Enter the number of seats in each row");
+		int b = scan.nextInt();
+		char ar[][] = greatArr(a,b);
+		System.out.println("Cinema:");
+		printAr(ar);
+		System.out.println("Enter a row number:");
+		int a1 = scan.nextInt();
+		System.out.println("Enter a seat number in that row:");
+		int b1 = scan.nextInt();
+		int price;
+		if(a*b < 60) {
+			price = 10;
+			}else {
+				if (a1 <= a/2) {
+					price = 10;
+				} else {
+					price = 8;
+				}
+			}
+		ar[a1][b1] = 'B';
+		System.out.printf("Ticket price: $%d\n", price);
+		System.out.println("Cinema:");
+		printAr(ar);
 	}
 
 }
